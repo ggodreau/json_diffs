4b705f093e1f4b68aeb5f72adb627f6ee5e3cdb7
ggodreau <godreau@gmail.com>
2019-01-09 13:46:10 -0600

    squashed merge of landingpage branch into master
    commit 1c1c55cd4f732983685665ef8914078d40104a36
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 13:43:34 2019 -0600
    
        landing page
    
          - almost working, calls teh pyfunc api and gets a 403
          - added submit button
    
    commit d6e13c1857955ceb69fac9e9601171d48afa5a55
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 13:19:01 2019 -0600
    
        added favicon
    
    commit 430e534dc5a7223fdf2a5fc3c070d2cbd95570cc
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 13:08:09 2019 -0600
    
        landing
    
          - working pass of application/json POST from the form field!!
    
    commit 112d735e02097d1b27448b245c3494f3733a66ea
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 12:13:13 2019 -0600
    
        landing
    
          - removed trailing slash requirement to api endpoint
    
    commit 111988dd92730f352b837ccc0030750ec91ac6e3
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 11:43:37 2019 -0600
    
        landing page
    
          - successful post demonstration from the form field!!! omg!!
    
    commit 0452aedb10189a9b296bdf6c641b7c83d67e6724
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 11:10:27 2019 -0600
    
        landing
    
          - successful call of external api given a query parameter
    
    commit 581bde840b4a900637dc0f73588115e151a5c136
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 10:34:47 2019 -0600
    
        landing page
    
          - parsing query params and calling external functions
    
    commit 498ac8da78a9b19f38fbcc37612f7f158987b44a
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 10:24:37 2019 -0600
    
        landing page
    
          - added cors
          - demonstrated parsing of params
          - note that you still need the trailing slash with this solution which suxxxxx
    
    commit e8ab330394ea2f411a464bb847ea2457ff37ef54
    Author: ggodreau <godreau@gmail.com>
    Date:   Wed Jan 9 09:34:04 2019 -0600
    
        landing
    
          - totally vanilla call to modularized firebase endpoint
          - https://codeburst.io/express-js-on-cloud-functions-for-firebase-86ed26f9144c
          - needed firebase and firebase-admin packages
          - dont forget the training slash needed at end of API endpoint
    
    commit 556d292ab9a03d8ad9534b7f324bf35dc49eb70a
    Author: ggodreau <godreau@gmail.com>
    Date:   Tue Jan 8 18:46:34 2019 -0600
    
        landing page
    
          - successfully called external function from main function
    
    commit cf55ca1e38c5dc7bda4a310e78837a843bd012a5
    Author: ggodreau <godreau@gmail.com>
    Date:   Tue Jan 8 18:05:59 2019 -0600
    
        landing page
    
          - working split on args, one calls the chainer api, the other prints the arg
    
    commit 06403bba5f7c67abbd4e4fd7c016d7ed5558d03b
    Author: ggodreau <godreau@gmail.com>
    Date:   Tue Jan 8 17:55:21 2019 -0600
    
        landingpage
    
          - successful use of request to call the chainer api and display a response
          - no javascript in the html yet
    
    commit c4280d293b0cdb778f264f2a1a3d7585de5d5e49
    Author: ggodreau <godreau@gmail.com>
    Date:   Tue Jan 8 17:40:52 2019 -0600
    
        Working on landing page
    
          - successful import of request
          - successful display of the input form
          - next step is to parse the input params

---
+++ landingpage/gacloud.js
@@ -0,0 +1,50 @@
+const request = require('request');
+const express = require('express');
+const cors = require('cors');
+const functions = require('firebase-functions');
+
+const app = express()
+let chainer = 'https://us-central1-imag-178020.cloudfunctions.net/chainer'
+
+app.use(cors({ origin: true }))
+app.get("/", (req, res) => {
+  let mes = req.query.message;
+  if ( mes == null ){
+    res.set('Content-Type', 'text/html');
+    res.status(200).send(new Buffer('<link rel="shortcut icon" href="https://raw.githubusercontent.com/ggodreau/json_diffs/landingpage/assets/troll.png" /><span><img src="https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png"><h1>trollin</h1></span><form action="https://us-central1-imag-178020.cloudfunctions.net/pyfunc" method="post" enctype="application/json"><input type="text" name="id1" placeholder="first url"><input type="text" name="id2" placeholder="second url"><input type="text" name="fmt" placeholder="html"><input type="submit" value="send it"></form>'));
+  }
+  else {
+    callChainer(req, res, mes);
+  }
+});
+
+// the call the form submission goes to
+app.post('*', (req, res) => {
+  let mes2 = req.body.text;
+  res.set('Content-Type', 'text/plain');
+  res.status(200).send(mes2);
+});
+
+// remove express trailing slash requirement from inbound url
+const landing = functions.https.onRequest((request, response) => {
+  if (!request.path) {
+    request.url = `/${request.url}` // prepend '/' to keep query params if any
+  }
+  return app(request, response)
+})
+
+module.exports = {
+  landing
+}
+
+// call external api
+callChainer = (req, res, mes) => {
+  request(chainer, function (err, response, body) {
+    if(err){
+      res.status(200).send('shit got fucked');
+    } 
+    else {
+      res.status(200).send(body);
+    }
+  }
+)};
